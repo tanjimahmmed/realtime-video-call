2d0f147b1f7e2de2b1d1d513f044c3ad336719b1
Md Tanjim Ahmmed <44721685+tanjimahmmed@users.noreply.github.com>
2025-05-11 15:36:58 +0600

    add onboarding functionality

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -96,3 +96,38 @@ export function logout(req, res) {
     res.clearCookie("jwt");
     res.status(200).json({success: true, message: "Logout successful"})
 }
+
+export async function onboard(req, res) {
+    try {
+        const userId = req.user._id;
+
+        const {fullName, bio, nativeLanguage, learningLanguage, location} = req.body;
+
+        if(!fullName || !bio || !nativeLanguage || !learningLanguage || !location) {
+            return res.status(400).json({
+                message: "All fields are required",
+                missingFields: [
+                    !fullName && "fullName",
+                    !bio && "bio",
+                    !nativeLanguage && "nativeLanguage",
+                    !learningLanguage && "learningLanguage",
+                    !location && "location"
+                ].filter(Boolean),
+            })
+        }
+
+        const updatedUser = await User.findByIdAndUpdate(userId, {
+            ...req.body,
+            isOnboarded: true,
+        }, {new: true})
+        if(!updatedUser) return res.status(404).json({message: "User not found"});
+        
+        
+
+
+        res.status(200).json({success: true, user: updatedUser});
+    }catch(error){
+        console.error("Onboarding error", error);
+        res.status(500).json({message: "Internal server error"})
+    }
+} 
