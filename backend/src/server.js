472e4105a59b7b7f48f049cf5bfa3ed7fa6d33ec
Md Tanjim Ahmmed <44721685+tanjimahmmed@users.noreply.github.com>
2025-05-12 20:25:08 +0600

    *add axios and transtack *add cors

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
 import cookieParser from "cookie-parser";
 
 import authRoutes from "./routes/auth.route.js";
@@ -13,6 +14,10 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT;
 
+app.use(cors({
+    origin: "https://localhost:5173", 
+    credentials: true, //allow frontend to send the cookies
+}));
 
 app.use(express.json());
 app.use(cookieParser());
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,10 +6,21 @@ import NotificationsPage from "./pages/NotificationsPage.jsx";
 import CallPage from "./pages/CallPage.jsx";
 import ChatPage from "./pages/ChatPage.jsx";
 import OnboardingPage from "./pages/OnBoardingPage.jsx";
-
-import {Toaster} from "react-hot-toast";
+import { useQuery } from '@tanstack/react-query';
+import { axiosInstance } from './lib/axios.js';
 
 const App = () => {
+  const {data:authData, isLoading, error} = useQuery({
+    queryKey: ["authUser"],
+    queryFn: async() => {
+      const res = await axiosInstance.get("/auth/me");
+      return res.data;
+    },
+    retry: false, // auth check
+  });
+  
+  const authUser = authData?.user;
+
   return (
     <div className=''>
       <Routes>
