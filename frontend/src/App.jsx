86ed7d0bcea20049e3d87a64123f396c63b02992
Md Tanjim Ahmmed <44721685+tanjimahmmed@users.noreply.github.com>
2025-05-12 12:55:17 +0600

    *add all pages *add daisyUI *add toaster

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,27 @@
-import React from 'react'
+import {Route, Routes} from 'react-router';
+import HomePage from "./pages/HomePage.jsx";
+import SignUpPage from "./pages/SignUpPage.jsx";
+import LoginPage from "./pages/LoginPage.jsx";
+import NotificationsPage from "./pages/NotificationsPage.jsx";
+import CallPage from "./pages/CallPage.jsx";
+import ChatPage from "./pages/ChatPage.jsx";
+import OnboardingPage from "./pages/OnBoardingPage.jsx";
+
+import {Toaster} from "react-hot-toast";
 
 const App = () => {
   return (
-    <div className='bg-red-800 h-screen text-3xl'>App</div>
+    <div className=''>
+      <Routes>
+        <Route path="/" element={<HomePage/>} />
+        <Route path="/signup" element={<SignUpPage/>} />
+        <Route path="/login" element={<LoginPage/>} />
+        <Route path="/notifications" element={<NotificationsPage/>} />
+        <Route path="/call" element={<CallPage/>} />
+        <Route path="/chat" element={<ChatPage/>} />
+        <Route path="/onboarding" element={<OnboardingPage/>} />
+      </Routes>
+    </div>
   )
 }
 
