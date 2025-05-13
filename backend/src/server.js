10b51730b29628be8b18eca9b6a798cb4df8abfd
Md Tanjim Ahmmed <44721685+tanjimahmmed@users.noreply.github.com>
2025-05-13 18:14:48 +0600

    add signup page

--- backend/src/server.js
+++ backend/src/server.js
@@ -15,7 +15,7 @@ const app = express();
 const PORT = process.env.PORT;
 
 app.use(cors({
-    origin: "https://localhost:5173", 
+    origin: "http://localhost:5173", 
     credentials: true, //allow frontend to send the cookies
 }));
 
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import {Route, Routes} from 'react-router';
+import {Navigate, Route, Routes} from 'react-router';
 import HomePage from "./pages/HomePage.jsx";
 import SignUpPage from "./pages/SignUpPage.jsx";
 import LoginPage from "./pages/LoginPage.jsx";
@@ -24,13 +24,13 @@ const App = () => {
   return (
     <div className=''>
       <Routes>
-        <Route path="/" element={<HomePage/>} />
-        <Route path="/signup" element={<SignUpPage/>} />
-        <Route path="/login" element={<LoginPage/>} />
-        <Route path="/notifications" element={<NotificationsPage/>} />
-        <Route path="/call" element={<CallPage/>} />
-        <Route path="/chat" element={<ChatPage/>} />
-        <Route path="/onboarding" element={<OnboardingPage/>} />
+        <Route path="/" element={authUser ? <HomePage/> : <Navigate to="/login"/>} />
+        <Route path="/signup" element={!authUser ? <SignUpPage/> : <Navigate to="/" />} />
+        <Route path="/login" element={!authUser ? <LoginPage/> : <Navigate to="/" />} />
+        <Route path="/notifications" element={authUser ? <NotificationsPage/> : <Navigate to="/login"/>} />
+        <Route path="/call" element={authUser ? <CallPage/> : <Navigate to="/login"/>} />
+        <Route path="/chat" element={authUser ? <ChatPage/> : <Navigate to="/login"/>} />
+        <Route path="/onboarding" element={authUser ? <OnboardingPage/> : <Navigate to="/login"/>} />
       </Routes>
     </div>
   )
